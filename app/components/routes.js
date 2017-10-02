5128079dd841b24f83f929009f2e6ef036d75b7d
riodude <emil.kais94@gmail.com>
2017-10-02 15:10:10 -0400

    local laptop changes before pull from desktop changes

--- app/components/routes.js
+++ app/components/routes.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { Route } from 'react-router';
 import App from './app.js'
 
+import Landingpage from './containers/Landingpage';
+
 export default (
-	<Route path="/" component={App} />
+	<Route path="/" component={App} >
+	
+	</Route>
 )
